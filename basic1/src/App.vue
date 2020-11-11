d50ae38538b8bee89e3a900eaabc543f80c35209
Tsuwa16 <tk.suwa16@gmail.com>
2020-11-11 22:19:42 +0900

    add-event

--- basic1/src/App.vue
+++ basic1/src/App.vue
@@ -1,17 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button v-on:click="onClick()">click</button>
+    {{clickResult}}
+
+    <br>
+    <input v-on:change="onChange($event)" placeholder="change">
+    {{changeResult}}
+
+    <br>
+
+    <input @input="onInput($event)" placeholder="input">
+    {{inputResult}}
+
+    <br>
+
+    <input @keyup="onKeyup($event)" placeholder="keyup">
+    {{keyupResult}}
+
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      clickResult:'',
+      changeResult:'',
+      inputResult:'',
+      keyupResult:''
+    }
+  },
+  methods:{
+    onClick(){
+      this.clickResult = "clicked";
+    },
+    onChange(e){
+      this.changeResult = e.target.value;
+    },
+    onInput(e){
+      this.inputResult = e.target.value;
+    },
+    onKeyup(e){
+      this.keyupResult = e.target.value;
+    }
   }
 }
 </script>
